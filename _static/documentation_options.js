6eea936a7acc1038cddbbb90a43a476bccdde875
albapa <albapa@users.noreply.github.com>
2023-06-08 11:02:47 +0000

    deploy: fc4be068c5558c3cda4a4f1583deef258fa1d0a1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c541187',
+    VERSION: 'fc4be06',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
